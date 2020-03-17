293a18ffd79df615c00fe5c377212fe1af8c9b88
dario-piotrowicz <dario.piotrowicz@gmail.com>
2020-03-17 09:18:43 +0000

    making course 18-holes

--- src/app/store/course.reducer.ts
+++ src/app/store/course.reducer.ts
@@ -6,9 +6,24 @@ const initialCourse: Course = {
     id: '18A',
     name: 'Standard 18 Course',
     holes: [
-        {idx: 1, number: 1, par: 5 },
-        {idx: 2, number: 2, par: 3 },
-        {idx: 3, number: 3, par: 1 }
+        {idx: 6, number: 1, par: 5 },
+        {idx: 15, number: 2, par: 3 },
+        {idx: 3, number: 3, par: 6 },
+        {idx: 4, number: 4, par: 3 },
+        {idx: 14, number: 5, par: 2 },
+        {idx: 17, number: 6, par: 4 },
+        {idx: 8, number: 7, par: 2 },
+        {idx: 16, number: 8, par: 3 },
+        {idx: 12, number: 9, par: 3 },
+        {idx: 1, number: 10, par: 3 },
+        {idx: 9, number: 11, par: 5 },
+        {idx: 13, number: 12, par: 1 },
+        {idx: 7, number: 13, par: 3 },
+        {idx: 18, number: 14, par: 3 },
+        {idx: 10, number: 15, par: 2 },
+        {idx: 2, number: 16, par: 6 },
+        {idx: 5, number: 17, par: 5 },
+        {idx: 11, number: 18, par: 3 }
     ],
     players: []
 };
